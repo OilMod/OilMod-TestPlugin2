76554ad01939b209142c83cde716abfddfa28a35
sirati97 <me@sirati97.de>
2019-02-10 23:16:20 +0000

    data recovered from broken laptop

--- src/main/java/org/oilmod/test/plugin2/FastAxe.java
+++ src/main/java/org/oilmod/test/plugin2/FastAxe.java
@@ -1,11 +1,17 @@
 package org.oilmod.test.plugin2;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.HumanEntity;
+import org.oilmod.api.items.ItemInteractionResult;
 import org.oilmod.api.items.OilItem;
 import org.oilmod.api.items.OilItemStack;
 import org.oilmod.api.items.type.IAxe;
 import org.oilmod.api.items.type.IDurable;
 import org.oilmod.api.items.type.IShovel;
+import org.oilmod.api.util.InteractionResult;
 import org.oilmod.api.util.OilKey;
 
 public class FastAxe extends OilItem implements IAxe, IDurable {
@@ -26,5 +32,29 @@ public class FastAxe extends OilItem implements IAxe, IDurable {
     @Override
     public int getMaxDurability() {
         return 100;
+    }
+
+    @Override
+    public InteractionResult onItemLeftClickOnBlock(OilItemStack stack, HumanEntity human, Location pos, boolean offhand, BlockFace facing, float hitX, float hitY, float hitZ) {
+        human.sendMessage("onItemLeftClickOnBlock");
+        return super.onItemLeftClickOnBlock(stack, human, pos, offhand, facing, hitX, hitY, hitZ);
+    }
+
+    @Override
+    public InteractionResult onItemUseOnBlock(OilItemStack stack, HumanEntity human, Location pos, boolean offhand, BlockFace facing, float hitX, float hitY, float hitZ) {
+        human.sendMessage("onItemUseOnBlock");
+        return super.onItemUseOnBlock(stack, human, pos, offhand, facing, hitX, hitY, hitZ);
+    }
+
+    @Override
+    public ItemInteractionResult onItemLeftClick(OilItemStack stack, World world, HumanEntity human, boolean offhand) {
+        human.sendMessage("onItemLeftClick");
+        return super.onItemLeftClick(stack, world, human, offhand);
+    }
+
+    @Override
+    public ItemInteractionResult onItemRightClick(OilItemStack stack, World world, HumanEntity human, boolean offhand) {
+        human.sendMessage("onItemRightClick");
+        return super.onItemRightClick(stack, world, human, offhand);
     }
 }
