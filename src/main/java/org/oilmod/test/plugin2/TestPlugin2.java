1dd926a9edc04108951d9588479b377ff7720a93
sirati97 <me@sirati97.de>
2019-11-19 11:19:36 +0900

    updated to latest plugin specification

--- src/main/java/org/oilmod/test/plugin2/TestPlugin2.java
+++ src/main/java/org/oilmod/test/plugin2/TestPlugin2.java
@@ -1,13 +1,10 @@
 package org.oilmod.test.plugin2;
 
-import org.bukkit.Material;
 import org.oilmod.api.OilMod;
 import org.oilmod.api.items.ItemRegistry;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.oilmod.api.items.crafting.InterchangeableCraftingIngredient;
 import org.oilmod.api.items.crafting.ItemCraftingFactory;
 import org.oilmod.api.items.crafting.OilCraftingRecipe;
-import org.oilmod.api.items.crafting.OilItemCraftingResult;
 import org.oilmod.api.items.crafting.VanillaOilCraftingResult;
 import org.oilmod.api.rep.providers.minecraft.MinecraftBlock;
 import org.oilmod.api.rep.providers.minecraft.MinecraftItem;
@@ -17,25 +14,32 @@ import java.util.Random;
 /**
  * Created by sirati97 on 16.01.2016.
  */
-public class TestPlugin2 extends JavaPlugin {
+public class TestPlugin2 extends OilMod {
     public static final Random rnd = new Random();
-    private ItemRegistry itemRegistry;
     private static TestPlugin2 instance;
-    private OilMod mod;
+
+    public TestPlugin2() {
+        super();
+        instance = this;
+    }
+
+
 
     @Override
-    public void onEnable() {
-        instance = this;
-        mod = new OilMod("oiltest2");
-        itemRegistry = new ItemRegistry(mod);
-        itemRegistry.register(new FastPickaxe(mod.createKey("fast_pickaxe")));
-        itemRegistry.register(new FastShovel(mod.createKey("fast_shovel")));
-        itemRegistry.register(new FastAxe(mod.createKey("fast_axe")));
-        itemRegistry.register(new BackpackItem(mod.createKey("backpack")));
-        itemRegistry.register(new StickFlintItem(mod.createKey("stick_flint")));
+    public void onRegisterItems(ItemRegistry itemRegistry) {
+        System.out.println(itemRegistry);
+        itemRegistry.register(new FastPickaxe(createKey("fast_pickaxe")));
+        itemRegistry.register(new FastShovel(createKey("fast_shovel")));
+        itemRegistry.register(new FastAxe(createKey("fast_axe")));
+        itemRegistry.register(new BackpackItem(createKey("backpack")));
+        itemRegistry.register(new StickFlintItem(createKey("stick_flint")));
+    }
+
+    @Override
+    public void onRegisterCraftingRecipes() {
 
         OilCraftingRecipe recipe;
-        recipe = ItemCraftingFactory.createShapedRecipe(mod.createKey("tester"), 1,2, new VanillaOilCraftingResult(MinecraftItem.ARROW.get().getItem()), new InterchangeableCraftingIngredient(MinecraftBlock.COBBLESTONE.get().getBlock().getItem(), MinecraftBlock.STONE.get().getBlock().getItem()), MinecraftBlock.OAK_PLANKS.get().getBlock().getItem());
+        recipe = ItemCraftingFactory.createShapedRecipe(createKey("tester"), 1,2, new VanillaOilCraftingResult(MinecraftItem.ARROW.get().getItem()), new InterchangeableCraftingIngredient(MinecraftBlock.COBBLESTONE.get().getBlock().getItem(), MinecraftBlock.STONE.get().getBlock().getItem()), MinecraftBlock.OAK_PLANKS.get().getBlock().getItem());
         ItemCraftingFactory.registerGlobal(recipe);
     }
 
