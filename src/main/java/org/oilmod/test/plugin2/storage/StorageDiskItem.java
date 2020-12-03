0a6ac8852a6a403ff80261f0baf6432f179ffd6a
sirati97 <TheSirati97@gmail.com>
2020-12-03 00:23:31 +0000

    make crafting api properly handle depletion and replacement of ingredients streamlime and beautify builder code

--- src/main/java/org/oilmod/test/plugin2/storage/StorageDiskItem.java
+++ src/main/java/org/oilmod/test/plugin2/storage/StorageDiskItem.java
@@ -2,16 +2,11 @@ package org.oilmod.test.plugin2.storage;
 
 import org.oilmod.api.inventory.InventoryFactory;
 import org.oilmod.api.inventory.ModInventoryObject;
-import org.oilmod.api.items.ItemInteractionResult;
 import org.oilmod.api.items.NMSItemStack;
 import org.oilmod.api.items.OilItem;
 import org.oilmod.api.items.OilItemStack;
 import org.oilmod.api.items.type.IUnique;
-import org.oilmod.api.rep.entity.EntityHumanRep;
 import org.oilmod.api.rep.providers.minecraft.MinecraftItem;
-import org.oilmod.api.rep.world.WorldRep;
-import org.oilmod.api.util.InteractionResult;
-import org.oilmod.test.plugin2.PortableInventoryFilter;
 
 
 public class StorageDiskItem extends OilItem implements IUnique {
